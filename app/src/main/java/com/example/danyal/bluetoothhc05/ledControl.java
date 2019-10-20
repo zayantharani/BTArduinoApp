9043b95a193e30dc3a1f2413c14122fcfb7d3a25
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2019-10-20 16:24:20 +0500

    receiver-part-trying

--- app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
+++ app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
@@ -6,8 +6,12 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -16,7 +20,12 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class ledControl extends AppCompatActivity {
@@ -90,8 +99,42 @@ public class ledControl extends AppCompatActivity {
                 Disconnect();
             }
         });
-    }
 
+        new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                byte[] buffer = new byte[1024];
+                int bytes;
+                try {
+                    bytes=btSocket.getInputStream().read(buffer);
+                    Log.d("tester", String.valueOf(bytes));
+                    Log.d("tester2", getString(btSocket.getInputStream()));
+                    Log.d("tester3",buffer.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    // Function to convert an Input Stream to String in Java
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static String getString(InputStream in) throws IOException
+    {
+        Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(reader);
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+        }
+        br.close();
+
+        return sb.toString();
+    }
     private void sendSignal ( String number ) {
         if ( btSocket != null ) {
             try {
@@ -102,6 +145,15 @@ public class ledControl extends AppCompatActivity {
         }
     }
 
+//    private void receiveSignal ( String number ) {
+//        if ( btSocket != null ) {
+//            try {
+//                btSocket.getOutputStream().write(number.toString().getBytes());
+//            } catch (IOException e) {
+//                msg("Error");
+//            }
+//        }
+//    }
     private void Disconnect () {
         if ( btSocket!=null ) {
             try {
