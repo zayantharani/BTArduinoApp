bd3b8280c64fd4e183a597ab410c9df32a178cf0
zayantharani <zayantharani@gmail.com>
2019-10-21 11:32:23 +0500

    ErrorInReceiving

--- app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
+++ app/src/main/java/com/example/danyal/bluetoothhc05/ledControl.java
@@ -4,21 +4,23 @@ import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -31,16 +33,22 @@ import java.util.UUID;
 
 public class ledControl extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5, btnDis;
+    Button autoButton, manualButton, zeroButton, oneButton, twoButton;
     String address = null;
-    TextView lumn;
+    TextView tempTextView;
+    EditText deltaTEditText;
+    ImageView bluetoothImageView;
+    static Context context;
+
     private ProgressDialog progress;
+    Switch onOffSwitch;
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     InputStream inputStream;
     OutputStream outputStream;
+    static boolean switchIsChecked;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,63 +58,116 @@ public class ledControl extends AppCompatActivity {
 
         setContentView(R.layout.activity_led_control);
 
-        btn1 = (Button) findViewById(R.id.button2);
-        btn2 = (Button) findViewById(R.id.button3);
-        btn3 = (Button) findViewById(R.id.button5);
-        btn4 = (Button) findViewById(R.id.button6);
-        btn5 = (Button) findViewById(R.id.button7);
-        btnDis = (Button) findViewById(R.id.button4);
-        lumn = (TextView) findViewById(R.id.textView2);
+        autoButton = (Button) findViewById(R.id.autoButton);
+        manualButton = (Button) findViewById(R.id.manualButton);
+        zeroButton = (Button) findViewById(R.id.zeroButton);
+        oneButton = (Button) findViewById(R.id.oneButton);
+        twoButton= (Button) findViewById(R.id.twoButton);
+        tempTextView = (TextView) findViewById(R.id.tempTextView);
+        deltaTEditText = (EditText) findViewById(R.id.deltaTEditText);
+        bluetoothImageView = (ImageView) findViewById(R.id.bluetoothImageView);
+        onOffSwitch = (Switch) findViewById(R.id.onOffSwitch);
+        context = this.getApplicationContext();
 
         new ConnectBT().execute();
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick (View v) {
-                sendSignal("1");
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    switchIsChecked = true;
+                    onOffSwitch.setText("On");
+                }
+                else
+                {
+                    switchIsChecked = false;
+                    onOffSwitch.setText("Off");
+                }
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        autoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                sendSignal("2");
+                if (switchIsChecked){
+                    deltaTEditText.setVisibility(View.VISIBLE);
+                    autoButton.setBackgroundColor(Color.GRAY);
+                    manualButton.setBackgroundResource(android.R.drawable.btn_default);
+                    zeroButton.setVisibility(View.INVISIBLE);
+                    oneButton.setVisibility(View.INVISIBLE);
+                    twoButton.setVisibility(View.INVISIBLE);
+                    tempTextView.setText("");
+
+                    byte b[] = {'<',1, 2, 3, 4,'>'};
+                    sendSignal(b);
+                }
+                else
+                    Toast.makeText(ledControl.this, "Please turn on the device", Toast.LENGTH_SHORT).show();
             }
         });
 
-        btn3.setOnClickListener(new View.OnClickListener() {
+        manualButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                sendSignal("3");
+                if (switchIsChecked){
+                    manualButton.setBackgroundColor(Color.GRAY);
+                    autoButton.setBackgroundResource(android.R.drawable.btn_default);
+                    deltaTEditText.setVisibility(View.INVISIBLE);
+                    zeroButton.setVisibility(View.VISIBLE);
+                    oneButton.setVisibility(View.VISIBLE);
+                    twoButton.setVisibility(View.VISIBLE);
+                    tempTextView.setText("");
+
+                    sendSignal("2>");
+                }
+                else
+                    Toast.makeText(ledControl.this, "Please turn on the device", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        btn4.setOnClickListener(new View.OnClickListener() {
+        zeroButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                sendSignal("4");
+                if (switchIsChecked){
+                    sendSignal("0");
+                }
+                else
+                    Toast.makeText(ledControl.this, "Please turn on the device", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        btn5.setOnClickListener(new View.OnClickListener() {
+        oneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                sendSignal("5");
+                if (switchIsChecked){
+                    sendSignal("1");
+                }
+                else
+                    Toast.makeText(ledControl.this, "Please turn on the device", Toast.LENGTH_SHORT).show();
             }
         });
 
-        btnDis.setOnClickListener(new View.OnClickListener() {
+        twoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                Disconnect();
+                if (switchIsChecked){
+                    sendSignal("2");
+                }
+                else
+                    Toast.makeText(ledControl.this, "Please turn on the device", Toast.LENGTH_SHORT).show();
             }
         });
+
+
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(10000);
+                    while (!isBtConnected)
+                        Thread.sleep(10000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -116,9 +177,20 @@ public class ledControl extends AppCompatActivity {
 
                     try {
                         bytes = inputStream.read(buffer);
+                        String data = new String(buffer);
+                        final String filterData;
                         Log.d("tester", String.valueOf(bytes));
-//                    Log.d("tester2", getString(btSocket.getInputStream()));
-                        Log.d("tester3", "" + buffer.toString());
+//                        Log.d("tester2", getString(btSocket.getInputStream()));
+                        Log.d("tester3", "" + data);
+                        filterData = data.substring(0,2);
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    tempTextView.setText(filterData);
+                                }
+                        });
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -156,6 +228,16 @@ public class ledControl extends AppCompatActivity {
         }
     }
 
+    private void sendSignal (byte [] number) {
+        if ( btSocket != null ) {
+            try {
+                outputStream.write(number);
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+
 //    private void receiveSignal ( String number ) {
 //        if ( btSocket != null ) {
 //            try {
@@ -186,7 +268,13 @@ public class ledControl extends AppCompatActivity {
 
         @Override
         protected  void onPreExecute () {
-            progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please Wait!!!");
+            try{
+                progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please Wait!!!");
+            }
+            catch (Exception e)
+            {
+                Log.e("Exception", e.getMessage());
+            }
         }
 
         @Override
@@ -219,6 +307,7 @@ public class ledControl extends AppCompatActivity {
             } else {
                 msg("Connected");
                 isBtConnected = true;
+                bluetoothImageView.setImageResource(R.drawable.ic_bluetooth_connected);
             }
 
             progress.dismiss();
